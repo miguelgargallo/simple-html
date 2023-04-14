80dbe8f6985f7ddef15644f4116f74bb217cc270
Miguel Gargallo <mgargallo16@gmail.com>
2023-04-14 16:37:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-    $("#miBoton").click(function() {
-      $("body").toggleClass("night");
-      $("header").toggleClass("night");
-      $("button").toggleClass("night");
-    });
+  $("#miBoton").click(function() {
+    $("body").toggleClass("night");
+    $("header").toggleClass("night");
+    $("button").toggleClass("night");
+    
+    // Cambia el texto del botón según el modo actual
+    if ($("body").hasClass("night")) {
+      $("#miBoton").text("Modo día");
+    } else {
+      $("#miBoton").text("Modo noche");
+    }
   });
-  
+});
